21d92a4768202dc4e8f25b3c6518efa2376c0e6f
ArchItalia <117321045+ArchItalia@users.noreply.github.com>
2022-11-04 01:12:35 +0000

    Update weather.sh

--- weather/scripts/weather.sh
+++ weather/scripts/weather.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+# Jonathan Sanfilippo
 
 # This script is to get weather data from openweathermap.com in the form of a json file
 # so that conky will still display the weather when offline even though it doesn't up to date
 
 # you can use this or replace with yours
-api_key=82005d27a116c2880c8f0fcb866998a0
+api_key=2880c8f0fcb866998a082005d27a116c
 # get your city id at https://openweathermap.org/find and replace
 city_id=2655603
 
